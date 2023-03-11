57af54f0b447ba29a27b2dfb86b978318d67e0a5
CamilleRungette <c.rungette@gmail.com>
2023-03-11 17:52:58 +0100

    test server

--- src/url.js
+++ src/url.js
@@ -1,6 +1,7 @@
-const url =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.REACT_APP_BACK_END;
+// const url =
+//   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+//     ? "http://localhost:3000"
+//     : process.env.REACT_APP_BACK_END;
 
+const url = "http://localhost:3000";
 export default url;
